a176d44987c070bc368015bdd2f76df455ab3d45
ann8ty <ann@torus.co>
2022-07-13 10:37:23 -0600

    add tax code to product

--- stripe/resource_stripe_product.go
+++ stripe/resource_stripe_product.go
@@ -70,6 +70,11 @@ func resourceStripeProduct() *schema.Resource {
 					"and will appear on your customer’s statement in capital letters. " +
 					"Non-ASCII characters are automatically stripped. It must contain at least one letter.",
 			},
+			"tax_code": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Description: "A tax code ID.",
+			}
 			"unit_label": {
 				Type:     schema.TypeString,
 				Optional: true,
@@ -118,6 +123,7 @@ func resourceStripeProductRead(_ context.Context, d *schema.ResourceData, m inte
 		}(),
 		d.Set("shippable", product.Shippable),
 		d.Set("statement_descriptor", product.StatementDescriptor),
+		d.Set("tax_code", product.TaxCode),
 		d.Set("unit_label", product.UnitLabel),
 		d.Set("url", product.URL),
 		d.Set("metadata", product.Metadata),
@@ -162,6 +168,9 @@ func resourceStripeProductCreate(ctx context.Context, d *schema.ResourceData, m 
 	}
 	if statementDescriptor, set := d.GetOk("statement_descriptor"); set {
 		params.StatementDescriptor = stripe.String(ToString(statementDescriptor))
+	}
+	if statementDescriptor, set := d.GetOk("tax_code"); set {
+		params.TaxCode = stripe.String(ToString(taxCode))
 	}
 	if unitLabel, set := d.GetOk("unit_label"); set {
 		params.UnitLabel = stripe.String(ToString(unitLabel))
@@ -225,6 +234,9 @@ func resourceStripeProductUpdate(ctx context.Context, d *schema.ResourceData, m 
 	}
 	if d.HasChange("statement_descriptor") {
 		params.StatementDescriptor = stripe.String(ExtractString(d, "statement_descriptor"))
+	}
+	if d.HasChange("tax_code") {
+		params.TaxCode = stripe.String(ExtractString(d, "tax_code"))
 	}
 	if d.HasChange("unit_label") {
 		params.UnitLabel = stripe.String(ExtractString(d, "unit_label"))
